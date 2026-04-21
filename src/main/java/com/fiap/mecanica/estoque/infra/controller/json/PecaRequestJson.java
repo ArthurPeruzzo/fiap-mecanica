4b7324562c0f956d242d3bb464fe8263902ed623
Arthur Peruzzo <arthurkohl0@gmail.com>
2026-04-21 10:26:11 -0300

    M-1 - criacao de peca

--- src/main/java/com/fiap/mecanica/estoque/infra/controller/json/PecaRequestJson.java
+++ src/main/java/com/fiap/mecanica/estoque/infra/controller/json/PecaRequestJson.java
@@ -1,5 +1,6 @@
 package com.fiap.mecanica.estoque.infra.controller.json;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.DecimalMin;
 import jakarta.validation.constraints.Min;
 import jakarta.validation.constraints.NotBlank;
@@ -7,12 +8,39 @@ import jakarta.validation.constraints.NotNull;
 
 import java.math.BigDecimal;
 
+@Schema(
+        name = "PecaRequestJson",
+        description = "Dados necessários para criar uma peca"
+)
 public record PecaRequestJson(
-        @NotBlank(message = "O nome deve ser preenchido") String nome,
-        @NotBlank(message = "A descrição deve ser preenchida") String descricao,
+        @Schema(
+                description = "Nome da peca",
+                example = "Biela"
+        )
+        @NotBlank(message = "O nome deve ser preenchido")
+        String nome,
+
+        @Schema(
+                description = "Descrição da peca",
+                example = "Transmite movimento do pistão ao virabrequim."
+        )
+        @NotBlank(message = "A descrição deve ser preenchida")
+        String descricao,
+
+        @Schema(
+                description = "Preço da peca",
+                example = "100.00"
+        )
         @NotNull(message = "O preço deve ser preenchido")
-        @DecimalMin(value = "0.01", message = "O preço deve ser maior que zero") BigDecimal preco,
+        @DecimalMin(value = "0.01", message = "O preço deve ser maior que zero")
+        BigDecimal preco,
+
+        @Schema(
+                description = "Quantidade da peca",
+                example = "100"
+        )
         @NotNull(message = "A quantidade em estoque deve ser preenchida")
-        @Min(value = 0, message = "A quantidade em estoque não pode ser negativa") Integer quantidadeEstoque
+        @Min(value = 0, message = "A quantidade em estoque não pode ser negativa")
+        Integer quantidadeEstoque
 ) {
 }
